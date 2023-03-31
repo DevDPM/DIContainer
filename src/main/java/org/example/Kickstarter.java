967f5237132ee713909e9761ab0bcba38df305ca
DevDPM <jansen.dpm@gmail.com>
2023-03-31 16:56:22 +0200

    refactored

--- src/main/java/org/example/Kickstarter.java
+++ src/main/java/org/example/Kickstarter.java
@@ -9,14 +9,13 @@ import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Kickstarter {
 
-    private static Map<String, Object> dependencyHolder = new HashMap<>();
-    private static Map<Class, Object> pilotHolder = new HashMap<>();
-
+    private static final Map<String, Object> enabledDependencies = new HashMap<>();
+    private static final Map<Class<?>, Object> classesToInject = new HashMap<>();
+    private static final ClassLoader classLoader = Kickstarter.class.getClassLoader();
 
     private Kickstarter() {
 
@@ -28,76 +27,105 @@ public class Kickstarter {
     }
 
     private static void ignite(String packagePath) throws ClassNotFoundException {
-        try {
-            ClassLoader classLoader = Kickstarter.class.getClassLoader();
-            String path = packagePath.replace('.', '/');
+        List<Class<?>> packagePathList = getPackagepath(packagePath, new ArrayList<>());
+        declareAnnotatedField(DependencyEnabler.class, Enable.class, packagePathList);
+        initializeAnnotatedField(DependencyInjector.class, Inject.class, packagePathList);
+    }
 
-            URL url = classLoader.getResource(path);
-
-            if (url == null)
-                return;
-
-            File folder = new File(url.getPath());
-            File[] fileArray = folder.listFiles();
-
-            for (int i = 0; i < fileArray.length; i++) {
-                String fileName = fileArray[i].getName();
-                int index = fileName.indexOf(".");
-
-                if (fileName.contains("$"))
-                    continue;
-
-                if (!fileName.contains(".class")) {
-                    String childPath = packagePath + "." + fileName;
-                    ignite(childPath);
-                    continue;
-                }
-
-                String className = fileName.substring(0, index);
-
-                String classNamePath = packagePath + "." + className;
-                Class<?> scanClass = Class.forName(classNamePath);
-
-                if (scanClass.getAnnotation(DependencyEnabler.class) != null) {
-
-                    for (Field field : scanClass.getDeclaredFields()) {
-                        if (field.getAnnotation(Enable.class) != null) {
-
-                            if (field.getType().isInterface()) {
-                                if (!field.getAnnotation(Enable.class).fullClassName().equals(Object.class)) {
-                                    Class<?> interfaceToClass = field.getAnnotation(Enable.class).fullClassName();
-                                    dependencyHolder.put(field.getName(), interfaceToClass.getConstructor().newInstance());
-                                }
-                            } else {
-                                dependencyHolder.put(field.getName(), field.getType().getConstructor().newInstance());
-                            }
-                        }
-                    }
-                } else if (scanClass.getAnnotation(DependencyInjector.class) != null) {
-                    if (scanClass.isInterface())
+    private static void initializeAnnotatedField(Class<DependencyInjector> parent, Class<Inject> child, List<Class<?>> packagePathList) {
+        packagePathList.stream().filter(foundClass -> foundClass.getAnnotation(parent) != null)
+                .forEach(foundClass -> {
+                    if (foundClass.isInterface())
                         return;
-                    pilotHolder.put(scanClass, scanClass.getConstructor().newInstance());
-                }
-            }
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                 NoSuchMethodException ex) {
-            throw new RuntimeException(ex);
-        }
-        for (Map.Entry<Class, ?> globalClass : pilotHolder.entrySet()) {
-            for (Field field : globalClass.getKey().getDeclaredFields()) {
-                if (field.getAnnotation(Inject.class) != null) {
+                    try {
+                        classesToInject.put(foundClass, foundClass.getConstructor().newInstance());
+                        initializeField(child, foundClass, classesToInject.get(foundClass));
+
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                             NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    private static void declareAnnotatedField(Class<DependencyEnabler> parent, Class<Enable> child, List<Class<?>> packagePathList) {
+        packagePathList.stream()
+                .filter(foundClass -> foundClass.getAnnotation(parent) != null)
+                .forEach(foundClass -> {
+                    Arrays.stream(foundClass.getDeclaredFields())
+                            .filter(field -> field.getAnnotation(child) != null)
+                            .forEach(field -> declareFieldAndCollect(child, field));
+                });
+    }
+
+    private static void initializeField(Class<Inject> child, Class<?> foundClass, Object classInstance) {
+            for (Field field : foundClass.getDeclaredFields()) {
+                if (field.getAnnotation(child) != null) {
                     field.setAccessible(true);
                     try {
-                        field.set(globalClass.getValue(), dependencyHolder.get(field.getName()));
+                        field.set(classInstance, enabledDependencies.get(field.getName()));
                     } catch (IllegalAccessException ex) {
                         throw new RuntimeException(ex);
                     }
                 }
             }
+    }
+
+    private static void declareFieldAndCollect(Class<Enable> child, Field field) {
+        if (field.getType().isInterface()) {
+            if (!field.getAnnotation(child).fullClassName().equals(Object.class)) {
+                Class<?> interfaceToClass = field.getAnnotation(child).fullClassName();
+                try {
+                    enabledDependencies.put(field.getName(), interfaceToClass.getConstructor().newInstance());
+                } catch (InstantiationException | IllegalAccessException |
+                         InvocationTargetException | NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } else {
+            try {
+                enabledDependencies.put(field.getName(), field.getType().getConstructor().newInstance());
+            } catch (InstantiationException | IllegalAccessException |
+                     InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    public static <T> T returnType(Class<T> controller) {
-        return (T) pilotHolder.get(controller);
+    private static List<Class<?>> getPackagepath(String startPackagePath, List<Class<?>> packagePathList) throws ClassNotFoundException {
+
+        String path = startPackagePath.replace('.', '/');
+        URL url = classLoader.getResource(path);
+
+        if (url == null)
+            return new ArrayList<>();
+
+        File folder = new File(url.getPath());
+        File[] fileArray = folder.listFiles();
+
+        for (int i = 0; i < fileArray.length; i++) {
+            String fileName = fileArray[i].getName();
+            int index = fileName.indexOf(".");
+
+            if (fileName.contains("$"))
+                continue;
+
+            if (!fileName.contains(".class")) {
+                String childPath = startPackagePath + "." + fileName;
+                ignite(childPath);
+                continue;
+            }
+
+            String className = fileName.substring(0, index);
+
+            String classNamePath = startPackagePath + "." + className;
+            Class<?> foundClass = Class.forName(classNamePath);
+            packagePathList.add(foundClass);
+        }
+        return packagePathList;
+    }
+
+    public static <T> T getInstanceOf(Class<T> classFileName) {
+        return (T) classesToInject.get(classFileName);
     }
 }
